e25be035f33945df042488345e9bddb068f88177
Joshua Taillon <joshua.taillon@nist.gov>
2019-03-15 15:05:52 -0400

    update backup script to be more general

--- gitlab_project_backup.sh
+++ gitlab_project_backup.sh
@@ -3,17 +3,17 @@
 # ------------------------------------------------------------------
 # [Author: Joshua Taillon <***REMOVED***>]
 #
-#   NexusMicroscopyLIMS Gitlab Backup
+#   Gitlab Backup Script
 #
 #   This script will use the Gitlab API to initiate a backup of the
-#   NexusMicroscopyLIMS project and save the downloaded repository
+#   a NIST Gitlab project and save the downloaded repository
 #   to the current directory following the Gitlab naming conventions.
 # ------------------------------------------------------------------
 
-VERSION=0.1.0
-SUBJECT="gitlab_nexuslims_backup"
+VERSION=0.1.1
+SUBJECT="gitlab_backup"
 LOG_FILE="gitlab_backup.log"
-PROJECT_URL="https://***REMOVED***/api/v4/projects/676"
+PROJECT_URL="https://***REMOVED***/api/v4/projects"
 
 function log() {
     if [[ ${HIDE_LOG} ]]; then
@@ -24,22 +24,28 @@ function log() {
 }
 
 function usage {
-    echo "usage: ${0} [-hv] [-t token]"
-    echo "  -h      display help"
-    echo "  -v      display version"
-    echo "  -q      quiet operation - does not print log to console"
-    echo "  -t token   A Gitlab personal access token with API access"
+    echo "usage: ${0} [-hv] [-t token] [-n project_id]"
+    echo "  -t token        a Gitlab personal access token with API access      (required)"
+    echo "  -n project_id   a Gitlab project id number                          (required)"
+    echo "  -q              quiet operation - does not print log to console     (optional)"
+    echo "  -h              display detailed help"
+    echo "  -v              display version"
     echo ""
 }
 
 function added_help {
     echo "This script will initiate and download a backup of the "
-    echo "NexusMicroscopyLIMS Gitlab project using the Gitlab API. A personal"
-    echo "access token with full API access must be supplied for authentication"
-    echo "and can be created at: "
+    echo "NIST Gitlab project using the Gitlab API. The project ID is a"
+    echo "numeric value, and can be found on the project homepage underneath"
+    echo "the project name and next to the avatar image."
+    echo ""
+    echo "A personal access token with full API access must also be supplied"
+    echo "for authentication and can be created at: "
     echo ""
     echo "    https://***REMOVED***profile/personal_access_tokens"
     echo ""
+    echo "Finally, this tool requires the jq tool for parsing json, so please"
+    echo "ensure that is installed before running the script."
 }
 
 function check_jq {
@@ -62,7 +68,7 @@ if [[ $# == 0 ]] ; then
     exit 1;
 fi
 
-while getopts ":t:qvh" optname
+while getopts ":n:t:qvh" optname
   do
     case "$optname" in
       "q")
@@ -71,6 +77,9 @@ while getopts ":t:qvh" optname
       "v")
         echo "Version $VERSION"
         exit 0;
+        ;;
+      "n")
+        project_id=${OPTARG}
         ;;
       "t")
         token=${OPTARG}
@@ -97,6 +106,15 @@ while getopts ":t:qvh" optname
 
 shift $(($OPTIND - 1))
 
+# Check to make sure mandatory "options" are supplied
+if [[ -z ${project_id} ]] || [[ -z ${token} ]]
+then
+    log "[E] Both -n and -t options must be supplied for this script to run"
+    echo ""
+    usage
+    exit 1
+fi
+
 # --- Locks -------------------------------------------------------
 LOCK_FILE=/tmp/${SUBJECT}.lock
 
@@ -117,18 +135,31 @@ check_jq
 log "-------------------------------------------"
 log "[I] Running ${0}"
 
-# POST request to start a backup:
+EXPORT_URL=${PROJECT_URL}"/"${project_id}"/export"
 
-# a 202 response was successful; a 401 is unauthorized
-resp=$(curl -s --request POST --header "PRIVATE-TOKEN:${token}" ${PROJECT_URL}"/export")
+
+# POST request to start a backup:
+resp=$(curl -s --request POST --header "PRIVATE-TOKEN:${token}" ${EXPORT_URL})
 log "[I] backup post response: ${resp}"
 
 # check if POST was successful:
-if echo ${resp} | jq '.message|contains("202")' | grep -q 'true'; then
+# a 202 response was successful; a 401 is unauthorized; 403 is forbidden
+if echo ${resp} | jq '.error|contains("404")' 2> /dev/null | grep -q 'true'; then
+    log "[E] Something has gone wrong; request path was invalid"
+    exit 1
+elif echo ${resp} | jq '.message|contains("401")' 2> /dev/null | grep -q 'true'; then
+    log "[E] There was an error authenticating with the supplied access token."
+    log "[E] Please check that it is correct and try again."
+    exit 1
+elif echo ${resp} | jq '.message|contains("403")' 2> /dev/null | grep -q 'true'; then
+    log "[E] The supplied access token is not authorized to access the"
+    log "[E] project ID: ${project_id}. Please check both values and try again."
+    exit 1
+elif echo ${resp} | jq '.message|contains("202")' 2> /dev/null | grep -q 'true'; then
     :
 else
-    echo "ERROR: There was an error initiating the export request. Please check"
-    echo "the validity of the supplied access token and try again."
+    log "[E] There was an error initiating the export request. Please check"
+    log "[E] the validity of the supplied access token and project ID and try again."
     exit 1
 fi
 
@@ -138,7 +169,7 @@ for i in {1..100}
 do
     sleep 1s   # sleep for 1 second to wait for export to finish
     # get export status:
-    resp=$(curl -s --header "PRIVATE-TOKEN:${token}" ${PROJECT_URL}"/export")
+    resp=$(curl -s --header "PRIVATE-TOKEN:${token}" ${EXPORT_URL})
     log "[I] [${i}/100] backup status response: ${resp}"
     if echo ${resp} | jq '.export_status|contains("started")' | grep -q 'true'; then
         # export has not completed yet, so continue to wait
@@ -159,7 +190,7 @@ if [[ "${COMPLETED}" == "false" ]]; then
     log "[E] wait for the email from Gitlab and download the exported project manually"
 else
     # use curl to download the completed backup to the current directory
-    resp=$(curl -OJL --header "PRIVATE-TOKEN:${token}" ${PROJECT_URL}"/export/download")
+    resp=$(curl -OJL --header "PRIVATE-TOKEN:${token}" ${EXPORT_URL}"/download")
     log "[I] backup download response: ${resp}"
 fi
 
