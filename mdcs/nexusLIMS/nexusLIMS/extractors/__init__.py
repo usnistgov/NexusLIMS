0ac36cb1bf2808f54b7b7a86433d732badbcb413
Joshua Taillon <joshua.taillon@nist.gov>
2019-05-18 14:28:24 -0400

    add some more docs and comment custom-styles.css

--- mdcs/nexusLIMS/nexusLIMS/extractors/__init__.py
+++ mdcs/nexusLIMS/nexusLIMS/extractors/__init__.py
@@ -1,1 +1,6 @@
+"""
+This module contains the code used to harvest metadata from various file types
+generated from instruments in the Electron Microscopy Nexus facility.
+"""
+
 from .quanta_tif import get_quanta_metadata
