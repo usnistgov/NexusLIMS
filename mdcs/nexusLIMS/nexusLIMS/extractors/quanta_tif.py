10de12f477c4f3883ccd854e693896560eed75f5
Joshua Taillon <joshua.taillon@nist.gov>
2020-01-22 14:20:25 -0700

    Basic parsing of quanta tif into "bulk" metadata dictionary (working with parse_metadata method, but no nx_meta yet)

--- mdcs/nexusLIMS/nexusLIMS/extractors/quanta_tif.py
+++ mdcs/nexusLIMS/nexusLIMS/extractors/quanta_tif.py
@@ -1,7 +1,38 @@
+#  NIST Public License - 2019
+#
+#  This software was developed by employees of the National Institute of
+#  Standards and Technology (NIST), an agency of the Federal Government
+#  and is being made available as a public service. Pursuant to title 17
+#  United States Code Section 105, works of NIST employees are not subject
+#  to copyright protection in the United States.  This software may be
+#  subject to foreign copyright.  Permission in the United States and in
+#  foreign countries, to the extent that NIST may hold copyright, to use,
+#  copy, modify, create derivative works, and distribute this software and
+#  its documentation without fee is hereby granted on a non-exclusive basis,
+#  provided that this notice and disclaimer of warranty appears in all copies.
+#
+#  THE SOFTWARE IS PROVIDED 'AS IS' WITHOUT ANY WARRANTY OF ANY KIND,
+#  EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED
+#  TO, ANY WARRANTY THAT THE SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY
+#  IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
+#  AND FREEDOM FROM INFRINGEMENT, AND ANY WARRANTY THAT THE DOCUMENTATION
+#  WILL CONFORM TO THE SOFTWARE, OR ANY WARRANTY THAT THE SOFTWARE WILL BE
+#  ERROR FREE.  IN NO EVENT SHALL NIST BE LIABLE FOR ANY DAMAGES, INCLUDING,
+#  BUT NOT LIMITED TO, DIRECT, INDIRECT, SPECIAL OR CONSEQUENTIAL DAMAGES,
+#  ARISING OUT OF, RESULTING FROM, OR IN ANY WAY CONNECTED WITH THIS SOFTWARE,
+#  WHETHER OR NOT BASED UPON WARRANTY, CONTRACT, TORT, OR OTHERWISE, WHETHER
+#  OR NOT INJURY WAS SUSTAINED BY PERSONS OR PROPERTY OR OTHERWISE, AND
+#  WHETHER OR NOT LOSS WAS SUSTAINED FROM, OR AROSE OUT OF THE RESULTS OF,
+#  OR USE OF, THE SOFTWARE OR SERVICES PROVIDED HEREUNDER.
+#
+import configparser as _cp
+import io as _io
+
+
 def get_quanta_metadata(filename):
     """
-    Returns the metadata (as a string) from a .tif file saved by the FEI Quanta
-    SEM in the Nexus Microscopy Facility
+    Returns the metadata (as a dictionary) from a .tif file saved by the FEI
+    Quanta SEM in the Nexus Microscopy Facility
 
     Parameters
     ----------
@@ -10,23 +41,28 @@ def get_quanta_metadata(filename):
 
     Returns
     -------
-    metadata : str
+    midct : dict
         The metadata text extracted from the file
     """
     with open(filename, 'rb') as f:
         content = f.read()
     metadata_bytes = content[content.find(b'[User]'):]
-    metadata = metadata_bytes.decode().replace('\r\n', '\n')
+    metadata_str = metadata_bytes.decode().replace('\r\n', '\n')
 
-    return metadata
+    buf = _io.StringIO(metadata_str)
+    config = _cp.ConfigParser()
+    # make ConfigParser respect upper/lowercase values
+    config.optionxform = lambda option: option
+    config.read_file(buf)
 
+    mdict = {}
 
-# if __name__ == '__main__':
-#     """
-#     These lines are just for testing. For real use, import the methods you
-#     need and operate from there
-#     """
-#     meta = get_quanta_metadata('/mnt/***REMOVED***/Quanta/***REMOVED***/171218 - '
-#                         'Evergreen Sprig - Quanta/quad1image_001.tif')
-#
-#     print(meta)
+    for itm in config.items():
+        if itm[0] == 'DEFAULT':
+            pass
+        else:
+            mdict[itm[0]] = {}
+            for k, v in itm[1].items():
+                mdict[itm[0]][k] = v
+
+    return mdict
