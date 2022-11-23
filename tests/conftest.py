209a9c9e1c4d45549071aebec5d4cf08c8f3976e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 12:36:29 +0000

    Adding tarfile member sanitization to extractall()

--- tests/conftest.py
+++ tests/conftest.py
@@ -57,7 +57,26 @@ def pytest_configure(config):
     when importing the instruments.py module (for instrument_db)
     """
     with tarfile.open(tars['DB'], 'r:gz') as tar:
-        tar.extractall(path=os.path.dirname(tars['DB']))
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=os.path.dirname(tars["DB"]))
 
 
 def pytest_sessionstart(session):
@@ -69,7 +88,26 @@ def pytest_sessionstart(session):
     """
     for _, tarf in tars.items():
         with tarfile.open(tarf, 'r:gz') as tar:
-            tar.extractall(path=os.path.dirname(tarf))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=os.path.dirname(tarf))
 
 
 def pytest_sessionfinish(session, exitstatus):
